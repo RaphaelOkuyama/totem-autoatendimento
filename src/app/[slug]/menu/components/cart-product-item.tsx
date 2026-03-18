5e6bf5f7c109639df87ed705e472ad76a5c826c1
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2026-03-18 03:36:12 -0300

    feat: improve CartProductItem layout and functionality with enhanced styling and button sizes

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext } from "react";
@@ -14,20 +16,27 @@ interface CartItemProps {
 const CartProductItem = ({ product }: CartItemProps) => {
   const { decreaseProductQuantity, increaseProductQuantity, removeProduct } =
     useContext(CartContext);
+
   return (
-    <div className="flex items-center justify-between">
+    <div className="relative flex items-center justify-between mb-5 last:mb-0">
       {/* ESQUERDA */}
       <div className="flex items-center gap-3">
         <div className="relative h-20 w-20 rounded-xl bg-gray-100">
-          <Image src={product.imageUrl} alt={product.name} fill />
+          <Image 
+            src={product.imageUrl} 
+            alt={product.name} 
+            fill 
+            className="object-contain"
+          />
         </div>
         <div className="space-y-1">
-          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+          <p className="max-w-[140px] truncate text-ellipsis text-xs">
             {product.name}
           </p>
           <p className="text-sm font-semibold">
             {formatCurrency(product.price)}
           </p>
+
           {/* QUANTIDADE */}
           <div className="flex items-center gap-1 text-center">
             <Button
@@ -35,7 +44,7 @@ const CartProductItem = ({ product }: CartItemProps) => {
               variant="outline"
               onClick={() => decreaseProductQuantity(product.id)}
             >
-              <ChevronLeftIcon />
+              <ChevronLeftIcon size={16} />
             </Button>
             <p className="w-7 text-xs">{product.quantity}</p>
             <Button
@@ -43,18 +52,20 @@ const CartProductItem = ({ product }: CartItemProps) => {
               variant="destructive"
               onClick={() => increaseProductQuantity(product.id)}
             >
-              <ChevronRightIcon />
+              <ChevronRightIcon size={16} />
             </Button>
           </div>
         </div>
       </div>
-      {/* BOTÃO DE DELETAR */}
+
+      {/* BOTÃO DE DELETAR FIXADO NA DIREITA */}
       <Button
-        className="h-7 w-7 rounded-lg"
+        className="absolute right-0 top-1/2 h-8 w-8 -translate-y-1/2 rounded-lg border-gray-200 text-gray-400"
         variant="outline"
+        size="icon"
         onClick={() => removeProduct(product.id)}
       >
-        <TrashIcon />
+        <TrashIcon size={16} />
       </Button>
     </div>
   );
